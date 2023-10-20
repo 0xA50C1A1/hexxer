6d5c891f1a41f8905cfebb608c195f918058f189
0xA50C1A1 <mage.wizard88@gmail.com>
2023-10-20 16:04:00 +0300

    Fix integer overflows in p_enemy.c

--- src/p_enemy.c
+++ src/p_enemy.c
@@ -602,7 +602,7 @@ void A_Chase(mobj_t *actor)
 	// turn towards movement direction if not there yet
 	//
 	if (actor->movedir < 8) {
-		actor->angle &= (7 << 29);
+		actor->angle &= (7u << 29);
 		delta = actor->angle - (actor->movedir << 29);
 		if (delta > 0) {
 			actor->angle -= ANG90 / 2;
@@ -904,28 +904,28 @@ void FaceMovementDirection(mobj_t *actor)
 {
 	switch (actor->movedir) {
 	case DI_EAST:
-		actor->angle = 0 << 24;
+		actor->angle = 0u << 24;
 		break;
 	case DI_NORTHEAST:
-		actor->angle = 32 << 24;
+		actor->angle = 32u << 24;
 		break;
 	case DI_NORTH:
-		actor->angle = 64 << 24;
+		actor->angle = 64u << 24;
 		break;
 	case DI_NORTHWEST:
-		actor->angle = 96 << 24;
+		actor->angle = 96u << 24;
 		break;
 	case DI_WEST:
-		actor->angle = 128 << 24;
+		actor->angle = 128u << 24;
 		break;
 	case DI_SOUTHWEST:
-		actor->angle = 160 << 24;
+		actor->angle = 160u << 24;
 		break;
 	case DI_SOUTH:
-		actor->angle = 192 << 24;
+		actor->angle = 192u << 24;
 		break;
 	case DI_SOUTHEAST:
-		actor->angle = 224 << 24;
+		actor->angle = 224u << 24;
 		break;
 	}
 }
@@ -1811,7 +1811,7 @@ void A_SerpentChase(mobj_t *actor)
 	// turn towards movement direction if not there yet
 	//
 	if (actor->movedir < 8) {
-		actor->angle &= (7 << 29);
+		actor->angle &= (7u << 29);
 		delta = actor->angle - (actor->movedir << 29);
 		if (delta > 0) {
 			actor->angle -= ANG90 / 2;
@@ -1989,7 +1989,7 @@ void A_SerpentWalk(mobj_t *actor)
 	// turn towards movement direction if not there yet
 	//
 	if (actor->movedir < 8) {
-		actor->angle &= (7 << 29);
+		actor->angle &= (7u << 29);
 		delta = actor->angle - (actor->movedir << 29);
 		if (delta > 0) {
 			actor->angle -= ANG90 / 2;
@@ -4113,7 +4113,7 @@ void A_FastChase(mobj_t *actor)
 	// turn towards movement direction if not there yet
 	//
 	if (actor->movedir < 8) {
-		actor->angle &= (7 << 29);
+		actor->angle &= (7u << 29);
 		delta = actor->angle - (actor->movedir << 29);
 		if (delta > 0) {
 			actor->angle -= ANG90 / 2;
