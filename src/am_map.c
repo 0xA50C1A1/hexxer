b5fafb8436955da8bc0033aceed04968a2ffb906
0xA50C1A1 <mage.wizard88@gmail.com>
2023-10-28 01:03:52 +0300

    Fix 'type specified missing' warning

--- src/am_map.c
+++ src/am_map.c
@@ -550,7 +550,7 @@ void AM_clearFB(int color)
 boolean AM_clipMline(mline_t *ml, fline_t *fl)
 {
 	enum { LEFT = 1, RIGHT = 2, BOTTOM = 4, TOP = 8 };
-	register outcode1 = 0, outcode2 = 0, outside;
+	int outcode1 = 0, outcode2 = 0, outside;
 	fpoint_t tmp;
 	int dx, dy;
 
